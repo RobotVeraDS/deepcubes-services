f655a9e12bb710bf9cc2d905bf145bc697bb9e02
sm-svyat <sm.svyat@yandex.ru>
2019-02-28 13:17:38 +0300

    Modified scripts for train models

--- scripts/train_intents.py
+++ scripts/train_intents.py
@@ -6,21 +6,18 @@ import json
 import pandas as pd
 
 from deepcubes.models import LogisticIntentClassifier
-from deepcubes.services.embedders import EmbedderFactory
-from deepcubes.services.utils import get_new_model_id
+from deepcubes_services.services.embedders import NetworkEmbedder
 
 
-def main(csv_path, lang, config_path):
+def main(csv_path, lang, config_path, model_id):
     config_parser = configparser.ConfigParser()
     config_parser.read(config_path)
 
     MODEL_STORAGE = config_parser.get('classifier-service', 'MODEL_STORAGE')
-    EMBEDDER_PATH = config_parser.get('classifier-service', 'EMBEDDER_PATH')
-
-    embedder_factory = EmbedderFactory(EMBEDDER_PATH)
-
+    EMBEDDER_URL = config_parser.get('classifier-service', 'EMBEDDER_PATH')
     LANG_TO_EMB_MODE = dict(config_parser['embedder'])
-    LANG_TO_TOK_MODE = dict(config_parser['tokenizer'])
+
+    embedder = NetworkEmbedder('{}/{}'.format(EMBEDDER_URL, LANG_TO_EMB_MODE[lang]))
 
     questions, answers = [], []
 
@@ -37,22 +34,18 @@ def main(csv_path, lang, config_path):
                 questions.append(question)
                 answers.append(answer)
 
-    embedder = embedder_factory.create(LANG_TO_EMB_MODE[lang])
     classifier = LogisticIntentClassifier(embedder)
-    tokenizer_mode = LANG_TO_TOK_MODE[lang]
-    classifier.train(answers, questions, tokenizer_mode)
-
-    new_model_id = get_new_model_id(MODEL_STORAGE)
+    classifier.train(questions, answers)
 
     clf_params = classifier.save()
-    clf_path = os.path.join(MODEL_STORAGE, '{}.cube'.format(new_model_id))
+    clf_path = os.path.join(MODEL_STORAGE, '{}.cube'.format(model_id))
 
     os.makedirs(MODEL_STORAGE, exist_ok=True)
     with open(clf_path, 'w') as out:
         out.write(json.dumps(clf_params))
 
-    if new_model_id is not None:
-        print('Created intent classifier model id: {}'.format(new_model_id))
+    if model_id is not None:
+        print('Created intent classifier model id: {}'.format(model_id))
 
 
 if __name__ == '__main__':
@@ -63,6 +56,7 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--csv_path', required=True)
     parser.add_argument('-l', '--lang', required=True)
     parser.add_argument('-c', '--config', required=True)
+    parser.add_argument('-m', '--model_id', required=True)
 
     args = parser.parse_args()
-    main(args.csv_path, args.lang, args.config)
+    main(args.csv_path, args.lang, args.config, args.model_id)
